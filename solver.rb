a9a310e49ebb1e404e97f86e2869e0d8a09f4b3c
Awais Amjed <awaisamjed171@gmail.com>
2022-05-19 13:38:01 +0500

    Implement factorial method using TDD

---
+++ solver.rb
@@ -0,0 +1,8 @@
+class Solver
+  def factorial(number)
+    raise RangeError, 'Number should not be negative' if number.negative?
+    return 1 if [1, 0].include?(number)
+
+    number * factorial(number - 1)
+  end
+end
---
+++ spec/solver_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../solver'
+
+describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+
+  context 'Given 1 to the factorial method' do
+    it 'should return 1 as the factorial' do
+      number = 1
+      expect(@solver.factorial(number)).to eql(1)
+    end
+  end
+
+  context 'Given any positive number to the factorial method' do
+    it 'should return the factorial of number' do
+      number = 5
+      expect(@solver.factorial(number)).to eql(120)
+    end
+  end
+
+  context 'Given 0 to the factorial method' do
+    it 'should return 1 as the factorial' do
+      number = 0
+      expect(@solver.factorial(number)).to eql(1)
+    end
+  end
+
+  context 'Given any negative number to the factorial method' do
+    it 'should raise an exception' do
+      number = -2
+      expect { @solver.factorial(number) }.to raise_error(RangeError, 'Number should not be negative')
+    end
+  end
+end
